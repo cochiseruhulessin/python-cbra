0fc54dd36a9b6378580db09b3e5ba656f8f66d95
Cochise Ruhulessin <cochise.ruhulessin@unimatrixone.io>
2023-02-19 23:06:52 +0100

    Add settings module

--- cbra/core/__init__.py
+++ cbra/core/__init__.py
@@ -6,14 +6,23 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+from typing import cast
+from typing import Any
+
 from pydantic import Field
 
 from .application import Application
 from .endpoint import Endpoint
 from .resource import *
 
+from unimatrix.conf import settings # type: ignore
+
+
+settings: Any = cast(Any, settings)
+
 
 __all__: list[str] = [
+    'settings',
     'Application',
     'Create',
     'Delete',
