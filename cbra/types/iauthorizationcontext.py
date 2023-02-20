ca3cdd1b4b0246c031a671864ec78757e9e31b1d
Cochise Ruhulessin <cochise.ruhulessin@unimatrixone.io>
2023-02-20 12:10:56 +0100

    Add IAuthorizationContext.subject property

--- cbra/types/iauthorizationcontext.py
+++ cbra/types/iauthorizationcontext.py
@@ -6,7 +6,18 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+from .isubject import ISubject
 
 
 class IAuthorizationContext:
     __module__: str = 'cbra.types'
+
+    @property
+    def subject(self) -> ISubject:
+        return self.get_subject()
+
+    def is_authenticated(self) -> bool:
+        return self.subject.is_authenticated()
+
+    def get_subject(self) -> ISubject:
+        raise NotImplementedError
