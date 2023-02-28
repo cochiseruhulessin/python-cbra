a15aecec631aada008fa7fa0cb86a76225cf5ecf
Cochise Ruhulessin <cochise.ruhulessin@unimatrixone.io>
2023-02-28 17:13:07 +0100

    Use consistent topics

--- cbra/ext/google/googlepubsubtransport.py
+++ cbra/ext/google/googlepubsubtransport.py
@@ -7,34 +7,35 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 """Declares :class:`GoogleTransport`."""
-from aorta.transport import GoogleTransport
-from aorta.models import Message
+from typing import Any
+
+from aorta.types import Envelope
+from aorta.ext.google import PubsubTransport
 
 
-class GooglePubsubTransport(GoogleTransport):
-    command_project: str
-    command_topic: str
-    events_project: str
-    events_topic: str
+class GooglePubsubTransport(PubsubTransport):
+    prefix: str
+    service_name: str
 
     def __init__(
         self,
         project: str,
-        command_topic: str,
-        events_topic: str,
-        command_project: str | None = None,
-        events_project: str | None = None
+        prefix: str,
+        service_name: str
     ):
-        super().__init__(project=project, topic_path=None)
-        self.command_project = command_project or project
-        self.command_topic = command_topic
-        self.events_project = events_project or project
-        self.events_topic = events_topic
+        super().__init__(
+            project=project,
+            topic=self.topic_factory,
+            retry_topic=f'{self.prefix}.retry.{service_name}'
+        )
+        self.prefix = prefix
+        self.service_name = service_name
 
-    def get_topics(self, message: Message):
-        topics = [self.events_topic, f'{self.events_topic}.{message.kind}']
-        project = self.events_project
-        if message.is_command():
-            project = self.command_project
-            topics = [self.command_topic]
-        return [self.client.topic_path(project, x) for x in topics]
+    def topic_factory(self, envelope: Envelope[Any]) -> list[str]:
+        topics: list[str] = [
+            f'{self.prefix}.events',
+            f'{self.prefix}.events.{envelope.kind}'
+        ]
+        if envelope.is_command():
+            topics = [f'{self.prefix}.commands.{self.service_name}']
+        return topics
