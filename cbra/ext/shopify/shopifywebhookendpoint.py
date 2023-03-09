d14572e24165b80377ca37758120feec41dd6be5
Cochise Ruhulessin <cochise.ruhulessin@unimatrixone.io>
2023-03-09 14:55:24 +0100

    Allow any type for overrides

--- cbra/ext/shopify/shopifywebhookendpoint.py
+++ cbra/ext/shopify/shopifywebhookendpoint.py
@@ -6,6 +6,8 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+from typing import Any
+
 from cbra.ext.webhooks import WebhookEndpoint
 
 from .shopifywebhookenvelope import ShopifyWebhookEnvelope
@@ -19,53 +21,53 @@ class ShopifyWebhookEndpoint(WebhookEndpoint):
     summary: str = "Shopify"
     tags: list[str] = ["Webhooks (Incoming)"]
 
-    def log_event(self, envelope: ShopifyWebhookEnvelope) -> None:
-        self.logger.info("Received event %s", envelope.event_name)
+    def log_event(self, dto: Any) -> None:
+        self.logger.info("Received event %s", self.envelope.event_name)
 
     async def on_orders_cancelled(
         self,
-        envelope: ShopifyWebhookEnvelope
+        dto: Any
     ) -> None:
-        self.log_event(envelope)
+        self.log_event(dto)
 
     async def on_orders_create(
         self,
-        envelope: ShopifyWebhookEnvelope
+        dto: Any
     ) -> None:
-        self.log_event(envelope)
+        self.log_event(dto)
 
     async def on_orders_delete(
         self,
-        envelope: ShopifyWebhookEnvelope
+        dto: Any
     ) -> None:
-        self.log_event(envelope)
+        self.log_event(dto)
 
     async def on_orders_edited(
         self,
-        envelope: ShopifyWebhookEnvelope
+        dto: Any
     ) -> None:
-        self.log_event(envelope)
+        self.log_event(dto)
 
     async def on_orders_fulfilled(
         self,
-        envelope: ShopifyWebhookEnvelope
+        dto: Any
     ) -> None:
-        self.log_event(envelope)
+        self.log_event(dto)
 
     async def on_orders_paid(
         self,
-        envelope: ShopifyWebhookEnvelope
+        dto: Any
     ) -> None:
-        self.log_event(envelope)
+        self.log_event(dto)
 
     async def on_orders_partially_fulfilled(
         self,
-        envelope: ShopifyWebhookEnvelope
+        dto: Any
     ) -> None:
-        self.log_event(envelope)
+        self.log_event(dto)
 
     async def on_orders_updated(
         self,
-        envelope: ShopifyWebhookEnvelope
+        dto: Any
     ) -> None:
-        self.log_event(envelope)
+        self.log_event(dto)
